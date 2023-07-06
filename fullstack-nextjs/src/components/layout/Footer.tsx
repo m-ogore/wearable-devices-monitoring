8b38555479ab47b6d299c63eab128d85b968eb23
Jimmy NSENGA <8685602+nsejim@users.noreply.github.com>
2023-07-06 22:25:44 +0200

    added charts library and show a demo line chart

--- fullstack-nextjs/src/components/layout/Footer.tsx
+++ fullstack-nextjs/src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 function Footer() {
   return (
-    <div className="absolute bg-slate-100 w-full py-4 bottom-0 text-center text-sm">
+    <div className="fixed bg-slate-100 w-full py-4 bottom-0 text-center text-sm">
         <footer className="w-full">
             <div className="flex justify-center space-x-8 mb-8">
                 <Image src="/aceiot_logo.jpeg" width={96} height={96} alt="aceiot" />
