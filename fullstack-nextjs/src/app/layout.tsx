5d078e5ffddbd2e8ba349f3ca5dfc9f84380dc2b
m-ogore <ogoremarv@gmail.com>
2023-08-17 22:05:02 +0200

    no much changes

--- fullstack-nextjs/src/app/layout.tsx
+++ fullstack-nextjs/src/app/layout.tsx
@@ -52,14 +52,14 @@ export default function RootLayout({
                       open ? "w-80" : "w-20 "
                     } bg-dark-purple h-screen p-5  pt-8 relative duration-300`}
                   >
-                    <img
+                    <img alt="image"
                       src="/assets/control.png"
                       className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
                        border-2 rounded-full  ${!open && "rotate-180"}`}
                       onClick={() => setOpen(!open)}
                     />
                     <div className="flex gap-x-4 items-center">
-                      <img
+                      <img alt="image"
                         src="/assets/logo.png"
                         className={`cursor-pointer duration-500 ${
                           open && "rotate-[360deg]"
@@ -83,7 +83,7 @@ export default function RootLayout({
                             index === 0 && "bg-light-white"
                           } `}
                         >
-                          <img src={`/assets/${Menu.src}.png`} />
+                          <img alt="image" src={`/assets/${Menu.src}.png`} />
                           <span className={`${!open && "hidden"} origin-left duration-200`}>
                             {Menu.title}
                           </span>
